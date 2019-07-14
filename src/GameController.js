741bd0f3404e84b96ac785397979509e8ad8a3ea
Alnur_Faisal <alnurfaisal@outlook.com>
2019-07-14 12:55:15 +0800

    Fix package dependencies issues

---
+++ src/GameController.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class GameController extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <div />;
+  }
+}
+
+export default GameController;
